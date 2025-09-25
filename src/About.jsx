00edeb48a813a16413851f5d243d83757765d4bb
kendr <mkendra917@gmail.com>
2025-09-25 12:37:54 -0400

    fixed issue where images weren't showing on netify

--- src/About.jsx
+++ src/About.jsx
@@ -2,6 +2,7 @@
 
 //About page
 import React from "react";
+import kendraphoto from "./assets/KendraPhoto.jpg";
 //Displays information about me with buttons leading the user to my portfolio, and resume.
 
 function About() {
@@ -10,7 +11,7 @@ function About() {
       <div className="about">
               <h2>About Me:</h2>
       <h4>My name is Kendra Riley Matthews, I'm a 20 year old software development student at Centennial College, I am in my second year and initially started programming because I enjoyed a python course in highschool, since then I have been studying programming for 3 years now. I have enjoyed and interacted with technology since a young age making this career path more appealing to me in the long run.</h4>
-      <img src="src\assets\KendraPhoto.jpg" height={500} width={400}></img>
+      <img src={kendraphoto} height={500} width={400}></img>
       </div>
       <div className="about2">
               <a href="/Kendra_Matthews_Resume.pdf"><button>View my Resume</button></a>
